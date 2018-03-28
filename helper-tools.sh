e674c4e1d52e93ec580e0854520778d373bfb258
root <root@192.168.1.100>
2018-03-29 00:08:26 +0530

    added helper-tools.sh kubeconfig-gen.sh

--- helper-tools.sh
+++ helper-tools.sh
@@ -1,51 +1,96 @@
-#rm /usr/local/bin/cfssl*
+#!/bin/bash
 
-rm /usr/local/bin/etcd
-rm -r /etc/etcd /var/lib/etcd
-rm /usr/local/bin/kube-apiserver
-rm /usr/local/bin/kube-controller-manager
-rm /usr/local/bin/kube-scheduler
-rm /usr/local/bin/kubectl
+POSITIONAL=()
+CLEANUP=NO
+STOP=NO
+while [[ $# -gt 0 ]]
+do
+key="$1"
 
-
-
-
-# /var/lib/kubernetes : ca.pem,ca-key.pem, kubernetes-key.pem, kubernetes.pem, encryption-config.yaml, kubeconfig
-rm /etc/systemd/system/etcd.service
-rm /etc/systemd/system/kube-apiserver.service
-rm /etc/systemd/system/kube-controller-manager.service
-rm /etc/systemd/system/kube-scheduler.service
-rm /etc/systemd/system/kubelet.service
-rm /etc/systemd/system/kube-proxy.service
-
-
-rm /etc/cni/net.d/99-loopback.conf
-rm /etc/cni/net.d/10-bridge.conf
-
-# /opt/cni ?? /etc/cni ??
-
-rm -r /etc/cni/net.d \
-    /opt/cni/bin \
-    /var/lib/kubelet \
-    /var/lib/kube-proxy \
-    /var/lib/kubernetes \
-    /var/run/kubernetes
-
-
-rm -r /opt/cri-containerd/ \
-    /usr/local/sbin/runc \
-    /usr/local/bin/crictl \
-    /usr/local/bin/containerd \
-    /usr/local/bin/containerd-stress \
-    /usr/local/bin/critest \
-    /usr/local/bin/containerd-release \
-    /usr/local/bin/containerd-shim \
-    /usr/local/bin/ctr \
-    /usr/local/bin/cri-containerd \
-    /etc/systemd/system/containerd.service \
-    /etc/systemd/system/cri-containerd.service \
-    /etc/crictl.yaml
+case $key in
+    -c|--cleanup)
+    CLEANUP=YES
+    shift # past argument
+    #shift # past value
+    ;;
+    -s|--stop-services)
+    STOP=YES
+    shift # past argument
+    #shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
 
 
+
+
+
+cleanup_files()
+{
+    rm /usr/local/bin/cfssl*
+
+    rm /usr/local/bin/etcd
+    rm -r /etc/etcd /var/lib/etcd
+    rm /usr/local/bin/kube-apiserver
+    rm /usr/local/bin/kube-controller-manager
+    rm /usr/local/bin/kube-scheduler
+    rm /usr/local/bin/kubectl
+
+
+
+
+    # /var/lib/kubernetes : ca.pem,ca-key.pem, kubernetes-key.pem, kubernetes.pem, encryption-config.yaml, kubeconfig
+    rm /etc/systemd/system/etcd.service
+    rm /etc/systemd/system/kube-apiserver.service
+    rm /etc/systemd/system/kube-controller-manager.service
+    rm /etc/systemd/system/kube-scheduler.service
+    rm /etc/systemd/system/kubelet.service
+    rm /etc/systemd/system/kube-proxy.service
+
+
+    rm /etc/cni/net.d/99-loopback.conf
+    rm /etc/cni/net.d/10-bridge.conf
+
+    # /opt/cni ?? /etc/cni ??
+
+    rm -r /etc/cni/net.d \
+        /opt/cni/bin \
+        /var/lib/kubelet \
+        /var/lib/kube-proxy \
+        /var/lib/kubernetes \
+        /var/run/kubernetes
+
+
+    rm -r /opt/cri-containerd/ \
+        /usr/local/sbin/runc \
+        /usr/local/bin/crictl \
+        /usr/local/bin/containerd \
+        /usr/local/bin/containerd-stress \
+        /usr/local/bin/critest \
+        /usr/local/bin/containerd-release \
+        /usr/local/bin/containerd-shim \
+        /usr/local/bin/ctr \
+        /usr/local/bin/cri-containerd \
+        /etc/systemd/system/containerd.service \
+        /etc/systemd/system/cri-containerd.service \
+        /etc/crictl.yaml
+
+}
+
+
+if [ $STOP == "YES" ]  ; then
+    echo "Stop services"
+    systemctl stop etcd kube-apiserver kube-controller-manager kube-scheduler containerd cri-containerd kube-proxy 
+fi
+
+if [ $CLEANUP == "YES" ]  ; then
+    echo "cleanup"
+    cleanup_files
+fi
 
--- kubeconfig-gen.sh
+++ kubeconfig-gen.sh
@@ -39,6 +39,24 @@ kubectl config set-context default \
     --kubeconfig=kube-proxy.kubeconfig
 kubectl config use-context default --kubeconfig=kube-proxy.kubeconfig
 
+echo "####### Generate the kubeconfig file for the kubectl admin  #####"
+kubectl config set-cluster kubernetes \
+    --certificate-authority=ca.pem \
+    --embed-certs=true \
+    --server=https://192.168.1.100:6443 \
+    --kubeconfig=admin-config.kubeconfig
+kubectl config set-credentials admin \
+    --client-certificate=admin.pem \
+    --client-key=admin-key.pem \
+    --embed-certs=true \
+    --kubeconfig=admin-config.kubeconfig
+kubectl config set-context kubernetes \
+    --cluster=kubernetes --user=admin \
+    --kubeconfig=admin-config.kubeconfig
+kubectl config use-context kubernetes \
+    --kubeconfig=admin-config.kubeconfig
+
+
 echo "######  Generate the data encryption key for   #####"
 ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
 cat > encryption-config.yaml << EOF
